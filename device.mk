781852b064a171eca4cd3a2f6b646d0ffd35e87a
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    lyriq: Inherit launch_with_vendor_ramdisk for vAB
    - mt6893 devices have a vendor_boot partition
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51
    Signed-off-by: Alvyrion <alvyrion0@gmail.com>

--- device.mk
+++ device.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit launch_with_vendor_ramdisk product
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
 DEVICE_PATH := device/motorola/lyriq
 
 # Shipping API level
