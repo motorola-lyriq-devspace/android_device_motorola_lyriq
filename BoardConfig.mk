b1b1b9882ae4a0570ab5395d4d14c6c690267a80
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-07-01 17:25:49 +0700

    lyriq: Enable updateable APEX
    Change-Id: Icd14570f0029853debe1977fc00a084d2db85d7c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 DEVICE_PATH := device/motorola/lyriq
 KERNEL_PATH := device/motorola/lyriq-kernel
 
+# APEX
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod
