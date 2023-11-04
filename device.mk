1fa18928bec19da4fa927ea76ae68051183628f0
AdarshGrewal <adarshgrewal@gmail.com>
2023-11-04 18:56:28 +0530

    lyriq: Setup AVB flags
    Change-Id: Iaf94bd8cbe10035dd8278a733f0e2aa4f9231cf5

--- device.mk
+++ device.mk
@@ -9,6 +9,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
