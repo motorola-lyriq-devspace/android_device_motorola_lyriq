68b0276152eed8358058ba9f1fb19c4b4221ffe7
Sushrut1101 <guptasushrut@gmail.com>
2025-04-15 20:08:14 +0530

    lyriq: Build common AIDL USB services
    Signed-off-by: Alvyrion <alvyrion0@gmail.com>

--- device.mk
+++ device.mk
@@ -83,5 +83,10 @@ PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
     hardware/mediatek
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/motorola/lyriq/lyriq-vendor.mk)
