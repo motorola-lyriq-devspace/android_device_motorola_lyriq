3cea830dc6682f485a07f9167b4d2fd96f24caed
claxten10 <claxten10@gmail.com>
2025-04-20 19:42:44 +0200

    lyriq: Initial skeleton
    Change-Id: I025839192b5f8d718aed5f0d6677e71519174762
    Signed-off-by: claxten10 <claxten10@gmail.com>

---
+++ device.mk
@@ -0,0 +1,16 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/motorola/lyriq
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/motorola/lyriq/lyriq-vendor.mk)
---
+++ lineage_lyriq.mk
@@ -0,0 +1,22 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/motorola/lyriq/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := lyriq
+PRODUCT_NAME := lineage_lyriq
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := motorola edge 40
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
