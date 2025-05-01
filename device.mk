fff33e4dfaa6b0d7034de19cc288b28e0b8fd9b3
claxten10 <claxten10@gmail.com>
2025-05-01 20:54:49 +0200

    lyriq: init: Import GS201 usb init
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -70,6 +70,7 @@ PRODUCT_PACKAGES += \
     init.mmi.rc \
     init.modem.rc \
     init.mt6893.rc \
+    init.mt6893.usb.rc \
     init.cgroup.rc \
     init.recovery.mt6893.rc \
     init.oem.hw.sh \
